02e4ca93f2a81257ec26bc4228d84c029f51fd5a
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-01-15 00:11:40 +0700

    hack around the mobile thing once and for all

--- demos/cylex/home.js
+++ demos/cylex/home.js
@@ -6,12 +6,12 @@
 const MAXPAGES = 3;
 
 // How much to scroll before we consider it a page move
-const THRESHOLD = 50;
+const THRESHOLD = 23;
 
 /** ------------- **/
 
 // current page number
-var page = 0;
+var page = -1, last_page = page, target;
 
 var is_scrolling = false;
 var scroll_target = window.innerHeight * page;
@@ -21,14 +21,58 @@ var touch_start = 0;
 // activate the JS-only styling
 document.body.classList.add("js");
 
+// we assume the sections will be static
+const sections = document.body.getElementsByTagName("section");
+
+// create script-only page indicator
+const scroller_parent = document.createElement("div");
+scroller_parent.id = "js-scroller";
+scroller_parent.style.opacity = 1;
+
+for (var i=0; i <= MAXPAGES; i++) {
+	var new_child = scroller_parent.appendChild(
+		document.createElement("div")
+	);
+	
+	new_child.dataset.page = i;
+	
+	// add jump to page event
+	new_child.addEventListener("click", (e)=>{
+		last_page = page;
+		target = parseInt(e.target.dataset.page);
+		doScrollPage(target);
+	});
+}
+
+document.body.appendChild(scroller_parent);
+
 // perform the page scroll
 function doScrollPage(p) {
-// calculate where to scroll to
-	scroll_target = window.innerHeight * page;
-	window.scrollTo({top:scroll_target, behavior:"smooth"});
-
+	if (page == p) return;
+// page stuff
+	for (var i=0, section; section = sections[i]; i++) {
+		section.classList.remove("-active");
+		section.classList.remove("-previous");
+			section.classList.remove("-inverse");
+		
+		if (i == p) {
+			section.classList.add("-active");
+			section.classList.remove("-previous");
+			if (last_page > p) {
+				section.classList.add("-inverse");
+			}
+		} else if (i == last_page) {
+			section.classList.remove("-active");
+			section.classList.add("-previous");
+			if (last_page > p) {
+				section.classList.add("-inverse");
+			}
+		}
+	}
+	
 // prevent overflow
-	if (page > MAXPAGES) page = MAXPAGES;
+	if (p > MAXPAGES) page = MAXPAGES;
+	else page = p;
 
 // change background color according to page counter
 	// page 0 will change the background color to that of CSS variable --page-1, etc.
@@ -44,23 +88,25 @@ function doScrollPage(p) {
 	scroller_parent.children[page].classList.add("_active");
 }
 
-// reset scroll position
-window.scrollTo({top:scroll_target, behavior:"smooth"});
+// init
+doScrollPage(0);
 
 // detects if the window is still scrolling
 window.addEventListener("scroll", (e)=>{
-	is_scrolling = !(window.scrollY == scroll_target);
+	window.scrollTo(0, 0);
 });
 
 // scrolljack
 window.addEventListener("wheel", (e)=>{
 	if (!is_scrolling) {
 		e.preventDefault();
+		last_page = page;
+		target = last_page;
 		
 		// simulate pageup/pagedown
-		if (e.deltaY > THRESHOLD) { if (page < MAXPAGES) page++; }
-		else if (e.deltaY < -THRESHOLD) { if (page > 0) page--; }
-		doScrollPage(page);
+		if (e.deltaY > THRESHOLD) { if (target < MAXPAGES) target++; }
+		else if (e.deltaY < -THRESHOLD) { if (target > 0) target--; }
+		doScrollPage(target);
 	}
 }, {
 	passive: false
@@ -75,36 +121,14 @@ window.addEventListener("touchend", (e)=>{
 	var delta = e.changedTouches[0].screenY - touch_start;
 	
 	if (!is_scrolling) {
+		last_page = page;
+		target = last_page;
 		// the directions are reversed here
-		if (delta < -THRESHOLD) { if (page < MAXPAGES) page++; }
-		else if (delta > THRESHOLD) { if (page > 0) page--; }
-		doScrollPage(page);
+		if (delta < -THRESHOLD) { if (target < MAXPAGES) target++; }
+		else if (delta > THRESHOLD) { if (target > 0) target--; }
+		doScrollPage(target);
 	}
 });
-
-// keep screen oriented
-window.addEventListener("resize", (e)=>{
-	scroll_target = window.innerHeight * page;
-	window.scrollTo({top:scroll_target});
-});
-
-// create script-only page indicator
-var scroller_parent = document.getElementById("js-scroller");
-scroller_parent.style.opacity = 1;
-
-for (var i=0; i <= MAXPAGES; i++) {
-	var new_child = scroller_parent.appendChild(
-		document.createElement("div")
-	);
-	
-	new_child.dataset.page = i;
-	
-	// add jump to page event
-	new_child.addEventListener("click", (e)=>{
-		page = parseInt(e.target.dataset.page);
-		doScrollPage(page);
-	});
-}
 
 // mark first page as active in the indicator
 scroller_parent.children[page].classList.add("_active");
